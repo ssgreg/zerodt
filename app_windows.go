60517d6ebb5de824842f74050ae0217564a619d7
Pavel Ivanov <pavel.ivanov@acronis.com>
2017-09-22 23:31:45 +0300

    Windows support added.

--- app_windows.go
+++ app_windows.go
@@ -6,22 +6,68 @@
 package zerodt
 
 import (
+	"context"
 	"net/http"
+	"time"
 )
 
 // TODO: use os.Interrupt
 
-// App TODO
+// App specifies functions to control passed HTTP servers.
 type App struct {
-	servers []*http.Server
+	PreServeFn         func(inherited bool) error
+	CompleteShutdownFn func()
+	PreParentExitFn    func()
+	servers            []*http.Server
 }
 
-// NewApp TODO
+// NewApp returns a new instance of App.
 func NewApp(servers ...*http.Server) *App {
-	return &App{servers}
+	return &App{nil, nil, nil, servers}
 }
 
-// Serve TODO
-func (a *App) Serve() error {
-	panic("Implement")
+// ListenAndServe calls ListenAndServe for all servers and returns first error if happens or nil.
+func (a *App) ListenAndServe() error {
+	errs := make(chan error)
+	for _, server := range a.servers {
+		server := server
+		go func() {
+			errs <- server.ListenAndServe()
+		}()
+	}
+
+	var err error
+	for i := 0; i < len(a.servers); i++ {
+		e := <-errs
+		if e != nil && e != http.ErrServerClosed && err == nil {
+			err = e
+		}
+	}
+
+	return err
+}
+
+// Shutdown calls Shutdown for all server and returns first error if happens or nil.
+func (a *App) Shutdown() error {
+	errs := make(chan error)
+	for _, server := range a.servers {
+		server := server
+		go func() {
+			errs <- server.Shutdown(context.Background())
+		}()
+	}
+
+	var err error
+	for i := 0; i < len(a.servers); i++ {
+		e := <-errs
+		if e != nil && err == nil {
+			err = e
+		}
+	}
+
+	return err
+}
+
+// SetWaitParentShutdownTimeout does nothing
+func (a *App) SetWaitParentShutdownTimeout(d time.Duration) {
 }
